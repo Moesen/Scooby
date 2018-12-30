cca5bbca5e1edae888239fde7c0318b2dd734078
s174169 <s174169@student.dtu.dk>
2018-12-30 13:01:27 +0100

    third try on musicify

---
+++ DiskretEksamen/src/Opgave4.java
@@ -0,0 +1,15 @@
+public class Opgave4 {
+
+    public static void main(String[] args) {
+
+        boolean cont = true;
+        int i = 1;
+        for(int j = 0; j < 10; j++){
+            System.out.println(12*(10+7*j)%21);
+        }
+
+    }
+
+
+
+}
